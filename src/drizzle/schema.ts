749deefd100f433e97332bfa63d7abb129f22a20
Maxnikit <max.nikit.03@gmail.com>
2024-11-10 21:08:20 +0300

    Add schema for Products

---
+++ src/drizzle/schema.ts
@@ -0,0 +1,26 @@
+import { index, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+const createdAt = timestamp("created_at", { withTimezone: true })
+  .notNull()
+  .defaultNow();
+const updatedAt = timestamp("updated_at", { withTimezone: true })
+  .notNull()
+  .defaultNow();
+
+export const ProductTable = pgTable(
+  "products",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkUserId: text("clerk_user_id").notNull(),
+    name: text("name").notNull(),
+    url: text("url").notNull(),
+    description: text("description"),
+    createdAt,
+    updatedAt,
+  },
+  (table) => ({
+    clerkUserIdIndex: index("products.clerk_user_id_index").on(
+      table.clerkUserId
+    ),
+  })
+);
