23e0373948d9ba242e030b3530adcdabe7439757
Maxnikit <max.nikit.03@gmail.com>
2024-11-10 21:03:01 +0300

    Setup drizzle

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,5 +1,6 @@
+import { env } from "@/data/env/server";
 import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
+const sql = neon(env.DATABASE_URL);
+export const db = drizzle(sql);
